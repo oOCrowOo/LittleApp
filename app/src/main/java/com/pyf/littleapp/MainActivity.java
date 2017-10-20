fbfaedce91b1dea5e1eb974b85dbcfdb8b4518d9
Yunfei Peng <pengyf@dhqtech.com>
2017-10-20 16:46:56 +0800

    add mybatis files

--- app/src/main/java/com/pyf/littleapp/MainActivity.java
+++ app/src/main/java/com/pyf/littleapp/MainActivity.java
@@ -2,12 +2,24 @@ package com.pyf.littleapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    String username;
+    String password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void Login(View view) {
+        EditText user_text = (EditText) findViewById(R.id.username);
+        EditText pwd_text = (EditText) findViewById(R.id.password);
+        username = user_text.getText().toString();
+        password = pwd_text.getText().toString();
     }
 }
---
+++ app/src/main/java/com/pyf/littleapp/cloud.java
@@ -0,0 +1,8 @@
+package com.pyf.littleapp;
+
+/**
+ * Created by yf_p on 2017/10/20.
+ */
+
+public class cloud {
+}
